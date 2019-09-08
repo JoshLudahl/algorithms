f09719f9b2967cdf3a274ed5bce2f07cc02d90ea
Joshy <Joshy@localhost.localdomain>
2019-09-08 16:50:13 -0700

    added binary for Int

---
+++ src/main/kotlin/Binary.kt
@@ -0,0 +1,34 @@
+
+class BinarySearch(private var element: Int) {
+
+    fun intSearch(list: List<Int>): Boolean {
+        var low = 0
+        var high = list.size-1
+        while (low <= high) {
+            var mid = (low + high)/2
+            var guess = list[mid]
+            if (guess == element) {
+                println("$element is in list!!!!")
+                return true
+            }
+            else if (guess > element) {
+                high = mid - 1
+            }
+            else {
+                low = mid + 1
+            }
+        }
+        return false
+    }
+}
+//  Execute Search ( this assumes list is sorted )
+fun main() {
+    var list = mutableListOf<Int>()
+
+    for (i in 1..1000) {
+        list.add(i)
+    }
+
+    var search = BinarySearch(699)
+    search.intSearch(list.sorted())
+}
