ec2271c2c946cc4b90fe94ba53d568609e1de1fd
JoshLudahl <josh.ludahl@gmail.com>
2019-09-08 17:51:56 -0700

    Added Selection Sort of integers.

---
+++ src/main/kotlin/SelectionSort.kt
@@ -0,0 +1,36 @@
+class SelectionSort {
+
+    //  Find the index of the smallest value
+    fun findSmallestIndex(list: List<Int>): Int {
+        var smallest = list[0]
+        var smallestIndex = 0
+
+        for (index in 0 until list.size) {
+            if (list[index] < smallest) {
+                smallest = list[index]
+                smallestIndex = index
+            }
+        }
+        return smallestIndex
+    }
+
+    fun sortArray(list: MutableList<Int>): List<Int> {
+        var newArray = mutableListOf<Int>()
+
+        for (index in 0 until list.size) {
+            var newSmallest = findSmallestIndex(list)
+            newArray.add(list[newSmallest])
+            list.removeAt(newSmallest)
+        }
+        return newArray
+    }
+}
+
+fun main() {
+    var list = mutableListOf<Int>(1, 2, 3, 6, 8, 10, 2, 9, 1, 7, 1, 12, 145, 3)
+    println("Pre Sorted: $list")
+    var selectionSort = SelectionSort()
+    var sortedList = selectionSort.sortArray(list)
+
+    println("Post Sorted: $sortedList")
+}
