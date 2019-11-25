32ef4510a9de7fbb7c7ecbdea418fbaa4ff686b2
JoshLudahl <josh.ludahl@gmail.com>
2019-11-24 19:15:10 -0800

    Added test coverage.

--- src/test/kotlin/BinaryTest.kt
+++ src/test/kotlin/BinaryTest.kt
@@ -22,6 +22,8 @@ internal class BinaryTest {
         assertEquals(true, binary.intSearch(listOf(6, 9)))
         assertEquals(true, binary.intSearch(listOf(1, 2, 5, 6)))
         assertEquals(true, binary.intSearch(listOf(2, 6, 7, 9)))
+        assertEquals(true, binary.intSearch(listOf(2, 3, 6, 7, 9, 10)))
+        assertEquals(true, binary.intSearch(listOf(1, 2, 3, 4, 6, 7, 9, 10)))
     }
 
     @Test
@@ -29,6 +31,8 @@ internal class BinaryTest {
         assertEquals(true, binary.intSearch(listOf(1, 6, 8)))
         assertEquals(true, binary.intSearch(listOf(6, 8, 9)))
         assertEquals(true, binary.intSearch(listOf(4, 5, 6)))
+        assertEquals(true, binary.intSearch(listOf(4, 5, 6, 7, 8, 9, 10)))
+        assertEquals(true, binary.intSearch(listOf(1, 2, 4, 5, 6, 9, 11)))
     }
 
     @Test
