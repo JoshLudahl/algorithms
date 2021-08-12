be7a5c8c92ebdb43cd96a01ccc91cd1ca0646ba9
Joshy <josh.ludahl@gmail.com>
2021-08-11 21:14:51 -0700

    formatting

--- src/main/kotlin/problems/leetcode/FindAllDuplicatesInArray.java
+++ src/main/kotlin/problems/leetcode/FindAllDuplicatesInArray.java
@@ -15,14 +15,11 @@ public class FindAllDuplicatesInArray {
             int index = Math.abs(nums[i]) - 1;
 
             // If the value is negative, we have seen this number before, add it to the list
-            if (nums[index] < 0) {
-                list.add(index + 1);
-            }
-
+            if (nums[index] < 0) list.add(index + 1);
+            
             // Set the current value of the index to a negative number to show we've seen it
             nums[index] *= -1;
         }
-
         return list;
     }
 }
